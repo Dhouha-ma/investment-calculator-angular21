f4915ec4331feea1052b80d7c3a9f1cd9af3b029
Dhouha <dhouha.mansour11@gmail.com>
2026-04-30 09:46:47 +0200

    add currency pipe to result data to display it in euro and enhance the table style

--- src/app/investment-calculator/components/calculation-result/calculation-result.ts
+++ src/app/investment-calculator/components/calculation-result/calculation-result.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 import { InvestmentResult } from '../../models/calculator.model';
 
 @Component({
   selector: 'app-calculation-result',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './calculation-result.html',
   styleUrl: './calculation-result.scss',
 })
